bcefea8bf604a44ffe63d1b3976a0a5c1b3ef837
mwae-bu <141448795+mwae-bu@users.noreply.github.com>
2024-03-21 22:41:32 -0400

    sample node code

---
+++ myfirst.js
@@ -0,0 +1,8 @@
+// Michael Waetzman, mwae@bu.edu
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World!');
+}).listen(8080); 
