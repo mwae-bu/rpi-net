e139349744dc0e38a236ecf07b0db6402671ad11
mwae-bu <141448795+mwae-bu@users.noreply.github.com>
2024-03-25 21:03:06 -0400

    Old bad

--- node.js
+++ node.js
@@ -6,12 +6,30 @@ var http = require('http'),
 
 fs.readFile('./index.html', function (err, html) {
     if (err) {
-        throw err; 
+        throw err;
     }
 
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
+    http.createServer(function(request, response) {
+        if (request.url === '/hls/stream') {
+            // Serve the HLS stream
+            const streamFile = 'http://192.168.1.101:3333';
+            fs.readFile(streamFile, (err, data) => {
+                if (err) {
+                    response.writeHead(500);
+                    response.end('Error loading HLS stream');
+                    return;
+                }
+
+                response.writeHead(200, {'Content-Type': 'application/vnd.apple.mpegurl'});
+                response.end(data);
+            });
+        } else {
+            // Serve the HTML page
+            response.writeHeader(200, {"Content-Type": "text/html"});
+            response.write(html);
+            response.end();
+        }
     }).listen(8080);
+
+    console.log('Server Running.');
 });
