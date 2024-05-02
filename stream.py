036add066ffda2e81e5c2439ff22ebfd3ad8d9ac
mwae-bu <mwae@bu.edu>
2024-05-02 11:15:33 -0400

    ...

--- stream.py
+++ stream.py
@@ -13,17 +13,17 @@ port = 5000
 app = Flask(__name__)
 
 def generate_frames():
-    with picamera.PiCamera as camera:
-        camera.resolution = (640, 480)  #! Pi Cam v2.1 supports 720p60 and 640x480p90
-        camera.framerate = 24
-        stream = io.BytesIO()
+    camera = picamera.PiCamera()
+    camera.resolution = (640, 480)  #! Pi Cam v2.1 supports 720p60 and 640x480p90
+    camera.framerate = 24
+    stream = io.BytesIO()
 
-        # Process frames and stream to app via JPEG stream
-        for _ in camera.capture_continuous(stream, 'jpeg', use_video_port=True):
-            stream.seek(0)  # wait to process
-            yield b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + stream.read() + b'r\\n'
-            stream.seek(0)
-            stream.truncate()
+    # Process frames and stream to app via JPEG stream
+    for _ in camera.capture_continuous(stream, 'jpeg', use_video_port=True):
+        stream.seek(0)  # wait to process
+        yield b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + stream.read() + b'r\\n'
+        stream.seek(0)
+        stream.truncate()
 
 @app.route('/video_feed')
 def video_feed():
